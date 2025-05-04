e655b7a463c1b211c750277aa1c12f19921ddaf9
Saurabh Sanwal <saurabhsanwal01@gmail.com>
2025-05-04 20:03:07 +0530

    correcting import on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Todo } from './components/todo';
+import {Todo} from "./components/Todo";
 
 const App = () => {
   return (
     <main>
-      <Todo />
+      <Todo/>
     </main>
   );
 };
